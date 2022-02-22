bd061a1184c0edfecd95e7a523f70abc4336fb58
AKomaty <akomaty@gmail.com>
2022-02-22 12:57:04 +0200

    Cleaned the workind dir

--- code.py
+++
@@ -1,5 +0,0 @@
-print("Hello I was in C2")
-print("Now I am in C3")
-print("This is the advanced programming course")
-print("I don't want to write any python programs")
-print("This is a demo for merge conflicts")
